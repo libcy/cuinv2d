b292e650a48af71ef70bc939f27f2f73292bab32
libccy <libccy@gmail.com>
2017-12-09 01:20:28 +0800

    set output path in argv

--- cuinv2d.cu
+++ cuinv2d.cu
@@ -3134,6 +3134,9 @@ int main(int argc, const char *argv[]){
     }
     else{
         datapath = argv[1];
+        if(argc >= 3){
+            dat::output_path = argv[2];
+        }
     }
     dat::parfile = datapath;
     clock_t timestart = clock();
